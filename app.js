8ca8915f2d015933d975ff399334d9feafe02a3e
Muhammad Ahmer Tahir <82140155+webmentordev@users.noreply.github.com>
2023-01-02 19:26:39 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const path = require('path');
+const server = http.createServer(app);
+const { Server } = require('socket.io')
+const io = new Server(server);
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+});
+
+io.on('connection', (socket) => {
+    console.log('user connected!');
+    socket.on('disconnect', () => {
+        console.log('User disconnected!');
+    });
+    socket.on('chat', (message) => {
+        console.log('message: ' + message);
+        io.emit('chat', message);
+    });
+})
+
+server.listen(3000, () => {
+    console.log('Server connected at http://localhost:3000');
+})
