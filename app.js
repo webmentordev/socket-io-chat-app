b6a544ce6fc3c446b1f3b63e28fba826550b0046
Muhammad Ahmer Tahir <82140155+webmentordev@users.noreply.github.com>
2023-01-08 23:30:05 +0500

    random user

--- app.js
+++ app.js
@@ -13,19 +13,33 @@ app.get('/', (req, res) => {
 });
 
 io.on('connection', (socket) => {
-    console.log('user connected!');
+    // Generate Random Number as User
+    const user = Math.floor(Math.random() * 3000000);
+
+    console.log('user connected!' + user);
+    
+    //User Randmon Number as userID
+    io.emit('user_info', user);
+
+    //Send message on user disconnect
     socket.on('disconnect', () => {
         console.log('User disconnected!');
     });
+
+    //Send message to the user
     socket.on('chat', (message) => {
         console.log('message: ' + message);
-        io.emit('chat', message);
+        io.emit('chat', message, user);
     });
+
+    //Check if user is typing
     socket.on('typing', (check) => {
         io.emit('typing', check);
     });
+
 });
 
+// Start CHAT-Server on Port 3000
 server.listen(3000, () => {
     console.log('Server connected at http://localhost:3000');
 });
